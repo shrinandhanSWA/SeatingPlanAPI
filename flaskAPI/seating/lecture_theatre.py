03474bc24d79389a6e33d255e56223cf8cf39a0c
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-11-05 20:44:22 +0000

    Refactor code and add comments
    Use comma to separate blocks in lecture theatre plan instead of spaces

--- flaskAPI/seating/lecture_theatre.py
+++ flaskAPI/seating/lecture_theatre.py
@@ -1,14 +1,27 @@
 from blocks.aisle import Aisle
 from blocks.door import Door
 from blocks.seat import Seat
-import random 
+import random
+
+
+def seats_are_neighbours(seat1, seat2):
+    r1, c1 = seat1.row, seat1.column
+    r2, c2 = seat2.row, seat2.column
+    return abs(r1 - r2) <= 1 and abs(c1 - c2) <= 1
+
 
 class LectureTheatre:
     def __init__(self, plan):
-        self.plan = ' '.join(filter(lambda s: s, plan.strip().split(' ')))
+        # remove
+        self.plan = plan.replace(" ", "")
+        print(self.plan)
+        # two-dimensional list consisting of blocks objects
         self.grid = []
+        # dictionary from seat number to Seat object
         self.seats = {}
+        # list of available seats
         self.available_seats = []
+
         self.initialise_grid()
 
     def no_of_columns(self):
@@ -18,10 +31,13 @@ class LectureTheatre:
         return len(self.grid)
 
     def initialise_grid(self):
+        """
+        Populates grid with block components based on lecture theatre plan
+        """
         default_seat_no = 0
         for i, row in enumerate(self.plan.split('\n')):
             row_blocks = []
-            for j, block in enumerate(row.strip().split(' ')):
+            for j, block in enumerate(row.split(',')):
                 if block == 'A':
                     row_blocks.append(Aisle())
 
@@ -30,7 +46,7 @@ class LectureTheatre:
 
                 elif block[0] == 'S':
                     seat_no_str = block[1:]
-
+                    # calculate seat number
                     if seat_no_str:
                         seat_no = int(seat_no_str)
                     else:
@@ -43,23 +59,29 @@ class LectureTheatre:
                     self.seats[seat_no] = seat
 
             self.grid.append(row_blocks)
-        
+
     def get_available_seats(self):
+        """
+        Filters all seats that have not been allocated yet
+        :return: list of seats
+        """
         return filter(lambda seat: seat.is_available(), self.seats.values())
 
     def allocate_random_seats(self, people):
+        """
+        Sets random occupant for available seats
+        :param people: list of people
+        :return: none
+        """
         random.shuffle(people)
 
         for seat, person in zip(self.get_available_seats(), people):
             seat.set_occupant(person)
 
-
-    def seats_are_neighbours(self, seat1, seat2):
-        r1, c1 = seat1.row, seat1.column
-        r2, c2 = seat2.row, seat2.column
-        return abs(r1 - r2) <= 1 and abs(c1 - c2) <= 1
-
     def block_neighbours(self, seat):
+        """
+        Block seats in front, behind, left and right of current seat
+        """
         row, column = seat.row, seat.column
 
         for dx in -1, 0, 1:
@@ -77,27 +99,32 @@ class LectureTheatre:
                 if isinstance(neighbour, Seat):
                     neighbour.set_unavailable()
 
+    def total_seats(self):
+        return len(self.seats)
+
     def block_alternate_seats(self):
+        """
+        Blocks alternate seats for social distancing
+        :return: none
+        """
         for seat_no in range(len(self.seats)):
             seat = self.seats[seat_no]
             if not seat.is_available():
                 continue
-
+            # block seats around the available seat
             self.block_neighbours(seat)
-            
+
+            # blocks the seat before and after current one
+            # if seats are in a diagonal
             if seat_no > 1:
                 left_seat = self.seats[seat_no - 1]
-                if self.seats_are_neighbours(left_seat, seat):
+                if seats_are_neighbours(left_seat, seat):
                     left_seat.set_unavailable()
 
-            if seat_no < len(self.seats) - 1:
+            if seat_no < self.total_seats() - 1:
                 right_seat = self.seats[seat_no + 1]
-                if self.seats_are_neighbours(seat, right_seat):
+                if seats_are_neighbours(seat, right_seat):
                     right_seat.set_unavailable()
-
-
-
-
 
     def __str__(self):
         return '\n'.join(map(lambda row: ''.join(map(str, row)), self.grid))
@@ -105,13 +132,14 @@ class LectureTheatre:
 
 if __name__ == "__main__":
     from people.student import Student
-    plan1 = '''
-        D  A   S S S S S S S S S S S  A  D
-        S S A   S S S S S S S S S S  A S S
-        S S S A  S S S S S S S S S  A S S S
-        S S S S A  S S S S S S S S A S S S S
-        S S S S  A  S S S S S S S A S S S S
-        S S S S   A  S S S S S S A S S S S
+
+    plan1 = '''\
+        D, A,  S,S,S,S,S,S,S,S,S,S,S, A, D
+        S,S,A,  S,S,S,S,S,S,S,S,S,S, A,S,S
+        S,S,S,A, S,S,S,S,S,S,S,S,S, A,S,S,S
+        S,S,S,S,A, S,S,S,S,S,S,S,S,A,S,S,S,S
+        S,S,S,S, A, S,S,S,S,S,S,S,A,S,S,S,S
+        S,S,S,S,  A, S,S,S,S,S,S,A,S,S,S,S\
     '''
     lt1 = LectureTheatre(plan1)
     people = []
