d0d30b3e7b18424b560c1f11b3277a06288391b3
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-11-05 08:56:49 +0000

    Add method to block alternate seats
    For social distancing

--- flaskAPI/seating/lecture_theatre.py
+++ flaskAPI/seating/lecture_theatre.py
@@ -11,26 +11,36 @@ class LectureTheatre:
         self.available_seats = []
         self.initialise_grid()
 
+    def no_of_columns(self):
+        return len(self.grid[0])
+
+    def no_of_rows(self):
+        return len(self.grid)
+
     def initialise_grid(self):
         default_seat_no = 0
-        for row in self.plan.split('\n'):
+        for i, row in enumerate(self.plan.split('\n')):
             row_blocks = []
-            for block in row.strip().split(' '):
+            for j, block in enumerate(row.strip().split(' ')):
                 if block == 'A':
                     row_blocks.append(Aisle())
+
                 elif block == 'D':
                     row_blocks.append(Door())
+
                 elif block[0] == 'S':
                     seat_no_str = block[1:]
+
                     if seat_no_str:
                         seat_no = int(seat_no_str)
                     else:
                         seat_no = default_seat_no
                         default_seat_no += 1
 
-                    seat = Seat(seat_no)
+                    seat = Seat(seat_no, i, j)
                     row_blocks.append(seat)
                     self.available_seats.append(seat)
+                    self.seats[seat_no] = seat
 
             self.grid.append(row_blocks)
         
@@ -39,6 +49,30 @@ class LectureTheatre:
 
         for seat, person in zip(self.available_seats, people):
             seat.set_occupant(person)
+
+
+    def seats_are_neighbours(self, seat1, seat2):
+        r1, c1 = seat1.row, seat1.column
+        r2, c2 = seat2.row, seat2.column
+        return abs(r1 - r2) <= 1 and abs(c1 - c2) <= 1
+
+    def block_alternate_seats(self):
+        for seat_no in range(len(self.available_seats)):
+            seat = self.seats[seat_no]
+            if not seat.is_available():
+                continue
+            if seat_no > 1:
+                left_seat = self.seats[seat_no - 1]
+                if self.seats_are_neighbours(left_seat, seat):
+                    left_seat.set_unavailable()
+
+            if seat_no < len(self.seats) - 1:
+                right_seat = self.seats[seat_no + 1]
+                if self.seats_are_neighbours(seat, right_seat):
+                    right_seat.set_unavailable()
+
+
+
 
 
     def __str__(self):
@@ -62,4 +96,5 @@ if __name__ == "__main__":
         people.append(Student("Rohan", "Pandit", "rp619"))
 
     lt1.allocate_random_seats(people)
+    lt1.block_alternate_seats()
     print(lt1)
