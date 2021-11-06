e467c6691e1adde7c939d9c84af4e77a7da2251e
metehanberker <metehanberker0@gmail.com>
2021-11-06 22:53:46 +0000

    some calls to database from the top of my head, to be filled and added more database calls

--- flaskAPI/queries.py
+++ flaskAPI/queries.py
@@ -1,1 +1,15 @@
-def
+def getListOfModules():
+    pass
+
+def getListOfStudentsTakingAModule(module):
+    pass
+
+def getStudent(student_id):
+    pass
+
+def getField(student_id):
+    pass
+
+def getLectureTheatre(lecture_theatre_id):
+    pass
+
