8481afd8e20f5f4790b504a20aa57de6c4ddcac9
sp2419 <sp2419@ic.ac.uk>
2021-11-22 19:17:16 +0000

    UPDATE app.py to handle lecture hall error correctly

--- app.py
+++ app.py
@@ -14,17 +14,18 @@ def index():
 
         seating = main(module, str(lecture_hall), filters)
 
+        if seating == -1:
+            result = {"status": "failure", "reason": "Lecture hall not found"}
+            return jsonify(result)
         if seating is not None:
             result = {"status": "success", "layout": seating}
-            return jsonify(result)
-        elif seating == -1:
-            result = {"status": "failure", "reason": "Lecture hall not found"}
             return jsonify(result)
         else:
             result = {"status": "failure", "reason": "Not enough seats for the students"}
             return jsonify(result)
     else:
         return jsonify({'Error': "This is a GET API method"})
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9007)
