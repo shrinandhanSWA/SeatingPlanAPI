237de7c3b5861a98241dcdce063435bc8e0818c9
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2022-01-07 19:13:34 +0000

    Skip dummy students on blank seats
    Refactor code

--- seating/subsection.py
+++ seating/subsection.py
@@ -1,14 +1,14 @@
 from seating.seat import Seat
-from seating.student import dummy_student
+from seating.student import DummyStudent, is_dummy_student
 
 
 class Subsection:
-    def __init__(self, rows, reqs, people, blanks):
+    def __init__(self, rows, reserved, people, blanks):
         self.valid = True
         self.seats = {}
-        self.rows = self.initialise_rows(blanks, people, reqs, rows)
+        self.rows = self.initialise_rows(blanks, people, reserved, rows)
 
-    def initialise_rows(self, blanks, people, reqs, rows):
+    def initialise_rows(self, blanks, people, reserved, rows):
         build = []
         for row in rows:
             if not self.valid:
@@ -21,18 +21,18 @@ class Subsection:
                 if str(count) in blanks:
                     # it is unavailable
                     # set it to the dummy person we have
-                    seat.set_occupant(dummy_student())
+                    seat.set_occupant(DummyStudent())
                     seat.set_unavailable()
                     this_available = False
 
-                if str(count) in reqs:
+                if str(count) in reserved:
                     if not this_available:
                         # trying to set student into a seat that was blanked out
                         self.valid = False
                         break
                     # find person and set occupant
                     for person in people:
-                        if person.get_name() == reqs[str(count)]:
+                        if person.get_name() == reserved[str(count)]:
                             seat.set_occupant(person)
                             seat.set_unavailable()
                             break
@@ -51,25 +51,28 @@ class Subsection:
     def is_valid(self):
         return self.valid
 
-    def allocate_seats(self, people):
+    def allocate_seats(self, students):
         """
         Sets random occupant for available seats
-        :param people: list of people
-        :return: list of people that could not be allocated
+        :param students: list of students
+        :return: list of students that could not be allocated
         """
 
         for row in self.rows:
             for seat in row:
                 if seat.is_available() and seat.get_seat_no() != -1:
-                    if not people:
+                    if not students:
                         return []
-                    person = people.pop()
+                    person = students.pop()
 
                     if person:
                         seat.set_occupant(person)
                         seat.set_unavailable()
 
-        return people
+                elif not seat.is_available() and is_dummy_student(students[-1]):
+                    students.pop()
+
+        return students
 
     def block_alternate_seats(self):
         """
